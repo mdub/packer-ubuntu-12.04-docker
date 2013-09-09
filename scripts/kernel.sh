7358b0c19e36f85754fab12bc80d3b3e0d2bbdf6
Mike Williams <mdub@dogbiscuit.org>
2013-09-09 12:23:49 +1000

    Use the AU Ubuntu mirror.

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# use AU mirror
+sed -i -e 's|http://archive.ubuntu.com|http://au.archive.ubuntu.com|g' /etc/apt/sources.list
+
 # install the backported kernel
 apt-get update
 apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
